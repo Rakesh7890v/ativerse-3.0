30dc92977d77e99cd5db5840bbe2fe007eeef445
Rakesh20456 <140162006+Rakesh20456@users.noreply.github.com>
2026-01-10 20:47:09 +0530

    changes on back

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,8 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb+srv://rishirakesh587_db_user:P89GrZ3GatR6Cysa@artiverse.st1etfp.mongodb.net/", {
   bufferCommands: false,
 })
 .then(() => console.log("MongoDB connected"))
